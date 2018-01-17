67bbf03c04b01056510381d36374b62bdc0f27dc
Filip Wieland <filiplamparski@gmail.com>
2018-01-17 21:05:49 +0000

    A working, building, running Android app starter

--- pb-android/src/main/kotlin/polybius/android/PlaybackControlActivity.kt
+++ pb-android/src/main/kotlin/polybius/android/PlaybackControlActivity.kt
@@ -10,33 +10,21 @@ import android.view.Menu
 import android.view.MenuItem
 
 class PlaybackControlActivity : AppCompatActivity() {
+    val toolbar by lazy { findViewById(R.id.toolbar) as Toolbar }
 
-    @Override
-    protected fun onCreate(savedInstanceState: Bundle) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playback_control)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener(object : View.OnClickListener() {
-            @Override
-            fun onClick(view: View) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show()
-            }
-        })
     }
 
-    @Override
-    fun onCreateOptionsMenu(menu: Menu): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_playback_control, menu)
         return true
     }
 
-    @Override
-    fun onOptionsItemSelected(item: MenuItem): Boolean {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
